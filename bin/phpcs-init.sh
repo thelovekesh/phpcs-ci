158b52058e3a54899e84e13ab4bde2a1744a8064
thelovekesh <lovekesh.kumar@rtcamp.com>
2023-03-03 21:24:38 +0530

    Add steps to install phpcs and it\'s standards from GitHub

--- bin/phpcs-init.sh
+++ bin/phpcs-init.sh
@@ -1,74 +1,104 @@
 #! /usr/bin/env bash
 
-set -e
+set -ex
 
 if [ "$USERNAME" == "root" ] ; then
 	echo "Will not run as root, exiting"
 	exit 1
 fi
 
-# Donwload from GitHub and unzip to a directory.
-function setup_from_release() {
-  local repo="$1"
-  local download_path="$2"
-  local unzip_path="$3"
+function loki_cleaner(){
+  local path_to_clean=$1
 
-  local repo_owner=${repo%/*}
-  local repo_name=${repo#*/}
-  local standard_name=${repo_name#*-}
+  local files_to_clean=(
+    '.git'
+    '.github'
+    '.gitignore'
+    '.gitattributes'
+    'composer.json'
+    'composer.lock'
+    'package.json'
+    'package-lock.json'
+    'CHANGELOG.md'
+    'phpcs.xml.dist.sample'
+    '.gitmodules'
+    '.travis.yml'
+    'phpcs.xml.dist'
+    'phpunit.xml.dist'
+    'README.md'
+    'tests'
+    'phpstan.neon.dist'
+    'phpstan.neon'
+    'CONTRIBUTING.md'
+    '.cspell.json'
+    '.coveralls.yml'
+    '.scrutinizer.yml'
+    'Tests'
+    'Test'
+    'docs'
+    '.phpcs.xml.dist'
+    '.phpdoc.xml.dist'
+    '.remarkrc'
+    '.remarkignore'
+    'psalm.xml'
+    'psalm-autoloader.php'
+    '.markdownlint-cli2.yaml'
+    '.yamllint.yml'
+    '.editorconfig'
+    'package.xml'
+    'CodeSniffer.conf.dist'
+  )
 
-  for var in repo_owner repo_name standard_name download_path unzip_path; do
-    if [ -z "${!var}" ]; then
-      echo "No $var specified"
-      exit 1
-    fi
-  done
+  for file in "${files_to_clean[@]}"; do
+    rm -rf $path_to_clean/$file
+  done;
 
-  local zip_url=$(curl --silent "https://api.github.com/repos/$repo_owner/$repo_name$/releases/latest" | jq -r '.zipball_url')
-
-  if [ -z "$zip_url" ] ; then
-    echo "No zip URL found"
-    exit 1
+  # Delete scripts dir from CodeSniffer
+  if [ -d "$path_to_clean/scripts" ] && [ -f "$path_to_clean/scripts/build-phar.php" ]; then
+    rm -rf "$path_to_clean/scripts"
   fi
 
-  local zip_file="$download_path/$standard_name.zip"
-
-  if [ -f "$zip_file" ] ; then
-    rm "$zip_file"
+  # Remove bin dir from Standards and not from CodeSniffer
+  if [ -d "$path_to_clean/bin" ] && { [ -f "$path_to_clean/bin/generate-forbidden-names-test-files" ] || [ -f "$path_to_clean/bin/php-lint" ] || [ -f "$path_to_clean/bin/pre-commit" ]; }; then
+    rm -rf "$path_to_clean/bin"
   fi
-
-  curl -L -o "$zip_file" "$zip_url"
-
-  if [ -d "$unzip_path" ] ; then
-    rm -rf "$unzip_path"
-  fi
-
-  unzip -q "$zip_file" -d "$unzip_path"
 }
 
-################################################################################
-#                                  Setup PHPCS                                 #
-################################################################################
-PHPCS_REPO="squizlabs/PHP_CodeSniffer/phpcs"
-PHPCS_STANDARDS_PATH="$HOME/phpcs/src/Standards"
+function loki_install_phpcs {
+  local phpcs_path="$1/phpcs" # Path to CodeSniffer
+  local phpcs_metadata_file="$2/phpcs.json" # Path to CodeSniffer which has repo and version info
+  local phpcs_standards_path="$phpcs_path/src/Standards" # Path to CodeSniffer Standards
+  local standards=$(jq -r '.standards | keys | .[]' $phpcs_metadata_file) # Standards to install
 
-setup_from_release "$PHPCS_REPO" "/tmp" "$HOME/phpcs"
+  # Delete old CodeSniffer
+  rm -rf $phpcs_path
 
-################################################################################
-#                             Setup PHPCS Standards                            #
-################################################################################
+  # Get CodeSniffer
+  local repo=$(jq -r '.phpcs.repo' $phpcs_metadata_file)
+  local version=$(jq -r '.phpcs.version' $phpcs_metadata_file)
 
-PHPCS_STANDARDS_REPO=(
-  'WPTT/WPThemeReview/WPThemeReview'
-  'PHPCSStandards/PHPCSUtils/PHPCSUtils'
-  'automattic/vip-coding-standards/vipwpcs'
-  'WordPress/WordPress-Coding-Standards/wpcs'
-  'phpcompatibility/phpcompatibility/php-compatibility'
-  'phpcompatibility/phpcompatibilitywp/phpcompatibility-wp'
-  'sirbrillig/phpcs-variable-analysis/phpcs-variable-analysis'
-  'phpcompatibility/phpcompatibilityparagonie/phpcompatibility-paragonie'
-)
+  git clone -c advice.detachedHead=false https://github.com/$repo.git $phpcs_path --branch $version --depth 1 --quiet
 
-for standard in "${PHPCS_STANDARDS_REPO[@]}" ; do
-  setup_from_release "$standard" "/tmp" "$PHPCS_STANDARDS_PATH"
-done
+  loki_cleaner $phpcs_path
+
+  installed_paths=''
+
+  # Get Standards
+  for standard in $standards; do
+    local repo=$(jq -r ".standards.\"$standard\".repo" $phpcs_metadata_file)
+    local version=$(jq -r ".standards.\"$standard\".version" $phpcs_metadata_file)
+
+    git -c advice.detachedHead=false clone https://github.com/$repo.git $phpcs_standards_path/$standard --branch $version --depth 1 --quiet
+
+    installed_paths+=",./src/Standards/$standard"
+
+    loki_cleaner $phpcs_standards_path/$standard
+  done;
+
+  # Set installed_paths using phpcs --config-set
+  $phpcs_path/bin/phpcs --config-set installed_paths $installed_paths
+}
+
+# $1 - Path to install CodeSniffer at.
+# $2 - Path to phpcs.json file.
+loki_install_phpcs $1 $2
